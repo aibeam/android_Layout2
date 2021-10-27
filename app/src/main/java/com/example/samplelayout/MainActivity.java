0365b55e1f4fd1140b6a07225bc23d7e8262b82f
aibeam <aibeam1025@gmail.com>
2021-10-27 10:52:51 +0900

    Initial commit

---
+++ app/src/main/java/com/example/samplelayout/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.samplelayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Layout1 layout1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        layout1 = findViewById(R.id.layout1);
+        layout1.setImage(R.mipmap.ic_launcher);
+        layout1.setName("홍홍홍");
+        layout1.setMobile("010-1234-5678");
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout1.setName("홍두깨");
+                layout1.setImage(R.mipmap.ic_launcher);
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout1.setName("홍삼이");
+                layout1.setImage(R.mipmap.ic_launcher_round);
+            }
+        });
+
+    }
+}
